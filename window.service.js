c66fc5a58578514ae3805b69643136da74f921b0
rishi-shah <31585886+rishi-shah@users.noreply.github.com>
2022-03-27 22:25:16 +0530

    Update window.service.js

--- window.service.js
+++ window.service.js
@@ -1,16 +1,11 @@
 var Service = require('node-windows').Service;
 
-// Create a new service object
 var svc = new Service({
   name:'Download Segregator',
   description: 'Segregates downloads into respective folders.',
   script: '<path-to-download-segregator>\\dist\\index.js',
-  //, workingDirectory: '...'
-  //, allowServiceLogon: true
 });
 
-// Listen for the "install" event, which indicates the
-// process is available as a service.
 svc.on('install',function(){
   svc.start();
 });
